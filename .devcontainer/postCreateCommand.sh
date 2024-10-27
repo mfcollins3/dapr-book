04d3e53a267a9d20241cc73dc0c7817a5423f4b3
Michael Collins <mfcollins3@me.com>
2024-10-26 18:54:55 -0700

    Generate ePUB version of the book
    I added mdbook-epub to the project. mdbook-epub will extend mdbook to
    generate an ePUB version of the book that can be installed in a reader
    program such as Apple Books on the iPad or macOS.
    
    I created a custom development container feature to install mdbook-epub
    and added it to the development container.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,16 +1,11 @@
+#!/usr/bin/env sh
+
 # Copyright 2024 Michael F. Collins, III
 #
 # Building Microservices with Dapr © 2024 by Michael F. Collins, III is
 # licensed under CC BY-ND 4.0. To view a copy of this license, visit
 # https://creativecommons.org/licenses/by-nd/4.0/
 
-[book]
-authors = ["Michael Collins"]
-language = "en"
-multilingual = false
-src = "src"
-title = "Building Microservices with Dapr"
+set -e
 
-[output.html]
-
-[output.epub]
+git config --global --add safe.directory /workspaces/dapr-book
